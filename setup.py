17f42a4f44556ccfd49d2fcf1d003b7fae7c7b69
Kyle Bilton <kjbilton@gmail.com>
2021-05-10 18:45:02 -0700

    Adding setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='radnets',
+    version='0.1.0',
+    description='Spectral gamma-ray detection with PyTorch',
+    author='Kyle J. Bilton',
+    author_email='kjbilton@gmail.com'
+    packages=['radnets'],
+)
