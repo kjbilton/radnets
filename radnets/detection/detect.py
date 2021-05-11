9054e759646bf3cb4b84b6540ce80629b387c12f
Kyle Bilton <kjbilton@gmail.com>
2021-05-10 20:47:57 -0700

    update detection

--- radnets/detection/detect.py
+++ radnets/detection/detect.py
@@ -4,23 +4,8 @@ Tools for performing binary anomaly detection for a single spectrum
 """
 import numpy as np
 import torch
-from ..data.preprocess import PREPROCESS, INVERSE_PREPROCESS
 
-def _preprocess(model, X, preprocess):
-    if preprocess in ['standardize', 'mean_center']:
-        Xhat = PREPROCESS[preprocess](X, model.mu, model.sigma)
-    else:
-        Xhat = PREPROCESS[preprocess](X)
-    return Xhat
-
-def _inv_preprocess(model, X, Xhat, preprocess):
-    if preprocess in ['standardize', 'mean_center']:
-        Xhat = INVERSE_PREPROCESS[preprocess](Xhat, model.mu, model.sigma)
-    elif preprocess in ['normlog']:
-        Xhat = INVERSE_PREPROCESS[preprocess](Xhat, X)
-    else:
-        Xhat = INVERSE_PREPROCESS[preprocess](Xhat)
-    return Xhat
+from .tools import _preprocess, _inv_preprocess
 
 def feedforward_deviance(model, X, preprocess):
     """
